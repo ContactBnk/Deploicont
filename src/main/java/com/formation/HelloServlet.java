eeda248316a751eb47aeda2eb2ede1a4e8f6941b
Bnm <contact.bnk@mailo.com>
2019-04-30 11:58:23 +0200

    commit initial bnm

---
+++ src/main/java/com/formation/HelloServlet.java
@@ -0,0 +1,25 @@
+package com.formation;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest requete, HttpServletResponse reponse) throws ServletException, IOException {
+		PrintWriter consoleServlet = reponse.getWriter();
+		consoleServlet = reponse.getWriter();
+	}
+
+	protected void doPost(HttpServletRequest requete, HttpServletResponse reponse) throws ServletException, IOException {
+		doGet( requete, reponse );
+	}
+
+}
